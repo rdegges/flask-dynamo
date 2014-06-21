34db6d7fb3fe56b6d1dbf712c4841c5695c6ed98
Randall Degges <r@rdegges.com>
2014-06-21 02:17:29 -0700

    Adding PyPI classifiers.

--- setup.py
+++ setup.py
@@ -39,4 +39,27 @@ setup(
     description = 'DynamoDB integration for Flask.',
     long_description = __doc__,
 
+    platforms = 'any',
+    classifiers = [
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: Public Domain',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Database',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.0',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+
 )
