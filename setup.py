b87c6758b942afbb57fc7d23a85d0ec39ada31a1
Randall Degges <r@rdegges.com>
2014-06-21 18:01:45 -0700

    Removing 2.6 support.

--- setup.py
+++ setup.py
@@ -71,7 +71,6 @@ setup(
         'Topic :: Database',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.0',
