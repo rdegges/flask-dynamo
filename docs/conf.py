f10ba662b39ee3b5380e3a6de14b010c9df53b32
Randall Degges <r@rdegges.com>
2014-06-21 20:26:56 -0700

    Adding proper logo (thanks @samanthadegges)!

--- docs/conf.py
+++ docs/conf.py
@@ -111,7 +111,7 @@ html_theme = 'flask'
 # further.  For a list of options available for each theme, see the
 # documentation.
 html_theme_options = {
-    'index_logo': 'flask-dynamo.jpg',
+    'index_logo': 'flask-dynamo.png',
 }
 
 # Add any paths that contain custom themes here, relative to this directory.
