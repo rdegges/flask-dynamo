d19904e36b328130d9f394f854ac4efb13c7ef9d
Austin Page <jaustinpage@gmail.com>
2017-08-21 13:21:33 -0500

    Update copyright year

--- docs/conf.py
+++ docs/conf.py
@@ -51,7 +51,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'flask-dynamo'
-copyright = u'2014, Randall Degges'
+copyright = u'2017, Randall Degges'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
